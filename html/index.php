dd6361627f5acd9f145b4549982961a101752bb2
8117070@ed.tus.ac.jp <yasuyasuiitoko@gmail.com>
2020-06-21 05:19:41 +0900

    環境構築

---
+++ html/index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8">
+    <title>aaaa</title>
+</head>
+
+<body>
+<?php $a="サンプル文章" ?>
+    <h1>サンプルで</h1>
+    <p><?php echo $a?></p>
+</body>
+</html>
